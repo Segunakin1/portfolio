b97ecc3b95b51f865b64004c8dfa5e85ba2eedc9
Segunakin1 <segunakinyemi07@gmail.com>
2022-08-25 09:56:57 +0100

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,10 +61,6 @@ sr.reveal('.about__profession', {delay: 400})
 sr.reveal('.about__text', {delay: 500})
 sr.reveal('.about__social-icon', {delay: 600, interval: 200})
 
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle', {})
-sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
-sr.reveal('.skills__img', {delay: 400})
 
 /*SCROLL PORTFOLIO*/
 sr.reveal('.portfolio__img', {interval: 200})
